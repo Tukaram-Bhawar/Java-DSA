09a96e310416246c6ae9ceb60f50d23308024c1e
Tukaram-Bhawar <tukarambhawar014496@gmail.com>
2025-01-17 20:22:16 +0530

    bellman foard algorith

--- belman_ford_algo.java
+++ belman_ford_algo.java
@@ -1,6 +1,50 @@
 public class belman_ford_algo {
+    public static void bellmanFord(int[][] edges, int V, int E, int src) {
+        int[] dist = new int[V];
+        for (int i = 0; i < V; ++i)
+            dist[i] = Integer.MAX_VALUE;
+        dist[src] = 0;
 
+        for (int i = 1; i < V; ++i) {
+            for (int j = 0; j < E; ++j) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int weight = edges[j][2];
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v])
+                    dist[v] = dist[u] + weight;
+            }
+        }
+
+        for (int j = 0; j < E; ++j) {
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int weight = edges[j][2];
+            if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+                System.out.println("Graph contains negative weight cycle");
+                return;
+            }
+        }
+
+        printArr(dist, V);
+    }
+
+    public static void printArr(int[] dist, int V) {
+        System.out.println("Vertex Distance from Source");
+        for (int i = 0; i < V; ++i)
+            System.out.println(i + "\t\t" + dist[i]);
+    }
+    
     public static void main(String[] args) {
-        int a=5;
+    int V = 5; // Number of vertices in graph
+    int E = 8; // Number of edges in graph
+
+    int[][] edges = { 
+        { 0, 1, -1 }, { 0, 2, 4 }, 
+        { 1, 2, 3 }, { 1, 3, 2 }, 
+        { 1, 4, 2 }, { 3, 2, 5 }, 
+        { 3, 1, 1 }, { 4, 3, -3 } 
+    };
+
+    bellmanFord(edges, V, E, 0);
     }
 }
